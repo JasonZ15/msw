d99d5f43de1d4d639150a6af1043e231be82c74b
JasonZ15 <vines15@gmail.com>
2011-10-30 14:31:42 +0800

    first commit

---
+++ layouts/blog.php
@@ -0,0 +1,96 @@
+<div class="box">
+	<?php 
+	if (is_paged()) $is_paged = true;
+	
+	if(get_option('woo_exclude_video') == "true")
+		$ex_vid = get_cat_id(get_option('woo_video_category'));
+	  
+	if (get_option('woo_slider_magazine_exclude') == "true") $exclude = get_option('woo_exclude'); // Get featured posts and exclude
+	
+	$args = array(	'post__not_in' => $exclude, 
+					'cat' => '-'.$ex_vid,
+					'paged'=> $paged ); 
+	
+	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1; query_posts($args);
+	
+	if (have_posts()) : $counter = 0;
+	while (have_posts()) : the_post(); $counter++;
+	?>
+		<div class="post-alt blog">
+            <?php if ( get_option('woo_blog_excerpt') == "true" ) { ?><div class="entry"><?php woo_image('width='.get_option('woo_home_thumb_width').'&class=alignright'); ?> </div><?php } ?>
+            
+			<h2><a title="<?php _e('Permanent Link to','woothemes'); ?> <?php the_title(); ?>" href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<div style="height: 16px;" onmouseover="setShare('<?php the_title(); ?>', '<?php the_permalink() ?>');">
+			<!-- JiaThis Button BEGIN -->
+<div id="ckepop">
+<!--<span class="jiathis_txt">分享到：</span>-->
+<a class="jiathis_button_twitter">Twitter</a>
+<a class="jiathis_button_fb">Facebook</a>
+<a class="jiathis_button_linkedin">LinkedIn</a>
+</div>
+<script type="text/javascript">
+function setShare(title, url) {
+jiathis_config.title = title;
+jiathis_config.url = url;
+}
+var jiathis_config = {
+url: "",
+title: ""
+}
+</script>
+<script type="text/javascript" >var jiathis_config={	data_track_clickback:true, siteNum:3,
+	sm:"twitter,fb,linkedin",	hideMore:true}</script> 
+<script type="text/javascript" src="http://v2.jiathis.com/code_mini/jia.js?uid=1539614" charset="utf-8"></script>
+<!-- JiaThis Button END -->
+			</div>
+			<p class="post_date"><?php _e('Posted on','woothemes'); ?> <?php the_time('d F Y'); ?> by <a rel="nofollow" href="<?php bloginfo('url'); ?>/authors/<?php the_author_firstname(); ?>-<?php the_author_lastname(); ?>"><b><?php the_author_link(); ?></b></a>. <a rel="nofollow" href="<?php bloginfo('url'); ?>/author/<?php the_author_firstname(); ?>">More posts</a> from <?php the_author_firstname(); ?>. <br /><span class="singletags"><?php if (function_exists('the_tags')) { ?><?php the_tags('Tags: ', ', ', ''); ?><?php } ?></span></p>
+			<div class="entry">
+				<?php if ( get_option('woo_blog_excerpt') == "true" ) { the_excerpt() ?>
+					<p><span class="continue"><a title="<?php _e('Permalink to ','woothemes'); ?> <?php the_title(); ?>" href="<?php the_permalink() ?>"><?php _e('Read the full story','woothemes'); ?></a></span></p>
+				<?php } else { the_content(__('<span class="continue">Continue Reading</span>','woothemes')); } ?>
+                
+			</div>
+			<div onmouseover="setShare('<?php the_title(); ?>', '<?php the_permalink() ?>');">
+			<!-- JiaThis Button BEGIN -->
+			
+<div id="jiathis_style_32x32">
+<a class="jiathis_button_delicious"></a>
+<a class="jiathis_button_reddit"></a>
+<a class="jiathis_button_tsina"></a>
+<a class="jiathis_button_tqq"></a>
+<a class="jiathis_button_renren"></a>
+<a href="http://www.jiathis.com/share?uid=1539614" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank"></a>
+</div>
+<script type="text/javascript">
+function setShare(title, url) {
+jiathis_config.title = title;
+jiathis_config.url = url;
+}
+var jiathis_config = {
+url: "",
+title: ""
+}
+</script><script type="text/javascript" >var jiathis_config={	data_track_clickback:true,	hideMore:false}</script>
+<script type="text/javascript" src="http://v2.jiathis.com/code/jia.js?uid=1539614" charset="utf-8"></script><br />
+<!-- JiaThis Button END -->
+</div>
+			 <br /><p class="posted"><?php _e('Posted in','woothemes'); ?> <?php the_category(', ') ?><span class="comments"><?php comments_popup_link(__('0 Comments','woothemes'), __('1 Comment','woothemes'), __('% Comments','woothemes')); ?></span></p>
+		
+		</div><!--/post-->		
+
+	<?php endwhile; ?>	
+    <?php endif; ?>	
+	
+	<div class="fix"></div>
+	
+    <div class="more_entries">
+        <?php if (function_exists('wp_pagenavi')) wp_pagenavi(); else { ?>
+            <div class="fl"><?php previous_posts_link(__('&laquo; Newer Entries ','woothemes')) ?></div>
+            <div class="fr"><?php next_posts_link(__(' Older Entries &raquo;','woothemes')) ?></div>
+            <br class="fix" />
+        <?php } ?> 
+    </div>		
+    
+    <div class="fix" style="height:15px"></div>
+
+</div>
---
+++ single.php
@@ -0,0 +1,74 @@
+<?php get_header(); ?>
+
+		<div class="col1">
+
+		<?php if (have_posts()) : ?>
+	
+			<?php while (have_posts()) : the_post(); ?>		
+
+				<div id="archivebox">
+					
+						<h3><em><?php _e('Categorized','woothemes'); ?> |</em> <?php the_category(', ') ?></h3>     
+				
+				</div><!--/archivebox-->			
+
+				<div class="post-alt blog" id="post-<?php the_ID(); ?>">
+				
+					<h2><a title="Permanent Link to <?php the_title(); ?>" href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>					<div style="height: 16px;" onmouseover="setShare('<?php the_title(); ?>', '<?php the_permalink() ?>');">					<!-- JiaThis Button BEGIN --><div id="ckepop"><!--<span class="jiathis_txt">���?��</span>--><a class="jiathis_button_twitter">Twitter</a><a class="jiathis_button_fb">Facebook</a><a class="jiathis_button_linkedin">LinkedIn</a></div><script type="text/javascript">function setShare(title, url) {jiathis_config.title = title;jiathis_config.url = url;}var jiathis_config = {url: "",title: ""}</script><script type="text/javascript" >var jiathis_config={	data_track_clickback:true, siteNum:3,	sm:"twitter,fb,linkedin",	hideMore:true}</script> <script type="text/javascript" src="http://v2.jiathis.com/code_mini/jia.js?uid=1539614" charset="utf-8"></script><!-- JiaThis Button END -->			</div>
+					<p class="post_date"><?php _e('Posted on','woothemes'); ?> <?php the_time('d F Y'); ?> by <a rel="nofollow" href="<?php bloginfo('url'); ?>/authors/<?php the_author_firstname(); ?>-<?php the_author_lastname(); ?>"><b><?php the_author_link(); ?></b></a>. <a rel="nofollow" href="<?php bloginfo('url'); ?>/author/<?php the_author_firstname(); ?>">More posts</a> from <?php the_author_firstname(); ?>. <span class="singletags"><?php if (function_exists('the_tags')) { ?><?php the_tags('Tags: ', ', ', ''); ?><?php } ?></span></p>
+                    
+                    <?php 
+                    if(woo_get_embed('embed',595,420))
+                        {   
+                        echo woo_get_embed('embed',595,420); 
+                        }
+		            ?>
+					<div class="entry">
+						<?php if (get_option('woo_image_single') == "false" && !woo_get_embed('embed',595,420)) woo_image('width='.get_option('woo_single_width').'&height='.get_option('woo_single_height').'&class=thumbnail alignright'); ?>
+						<?php the_content(__('<span class="continue">Continue Reading</span>','woothemes')); ?>
+					</div>
+				
+				</div><!--/post-->
+				
+               	<?php if (get_option('woo_author') == "true") { ?>
+
+                <div class="author_info">
+                    <h3><?php _e('About this author','woothemes'); ?>: <?php the_author_posts_link(); ?></h3>                	
+                    
+                        <?php
+                            // Determine which gravatar to use for the user
+                            $email = get_the_author_email();
+                            $grav_url = "http://www.gravatar.com/avatar.php?gravatar_id=".md5($email). "&default=".urlencode($GLOBALS['defaultgravatar'] )."&size=48";
+                            $usegravatar = get_option('woo_gravatar');
+                        ?>
+                        
+                    <?php  if ( $usegravatar ) { ?><span class="author_photo"><img src="<?php echo $grav_url; ?>" width="48" height="48" alt="" /></span><?php } ?>
+                    <!--<p> - who has written <?php the_author_posts(); ?> posts on <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>.</p>-->
+                    <p><?php the_author_description(); ?> <br style="clear:both;" /></p>
+                    <!--<p class="author_email"><a href="mailto:<?php the_author_email(); ?>">Contact the author</a></p>-->
+                </div>
+
+				<?php } ?>
+                
+                <div class="navigation">
+					<?php if (function_exists('woo_postnav')) woo_postnav(); else { ?>
+            			<div class="alignleft"><?php previous_posts_link(__('&laquo; Newer Entries ','woothemes')) ?></div>
+            			<div class="alignright"><?php next_posts_link(__(' Older Entries &raquo;','woothemes')) ?></div>
+            			<br class="fix" />
+           		 	<?php } ?>
+				</div>
+
+				<div id="comment">
+					<?php comments_template(); ?>
+				</div>
+
+		<?php endwhile; ?>
+		
+	
+	<?php endif; ?>							
+
+		</div><!--/col1-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
