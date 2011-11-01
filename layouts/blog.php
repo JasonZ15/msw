6f2ada3152e4bf739da363c6c843d5660fde4fc5
JasonZ15 <vines15@gmail.com>
2011-11-01 21:37:10 +0800

    gavatar profile pic post padding

--- layouts/blog.php
+++ layouts/blog.php
@@ -43,7 +43,7 @@ title: ""
 <script type="text/javascript" src="http://v2.jiathis.com/code_mini/jia.js?uid=1539614" charset="utf-8"></script>
 <!-- JiaThis Button END -->
 			</div>
-			<p class="post_date"><?php _e('Posted on','woothemes'); ?> <?php the_time('d F Y'); ?> by <b><?php the_author_posts_link(); ?></b>. <span class="singletags"><?php if (function_exists('the_tags')) { ?><?php the_tags('Tags: ', ', ', ''); ?><?php } ?></span></p>
+			<p class="post_date"><a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>" class="avatar_link"><?php echo get_avatar( get_the_author_meta('ID'), 32 ); ?></a><?php _e('Posted on','woothemes'); ?> <?php the_time('d F Y'); ?> by <b><?php the_author_posts_link(); ?></b>.<br /> <span class="singletags"><?php if (function_exists('the_tags')) { ?><?php the_tags('Tags: ', ', ', ''); ?><?php } ?></span></p>
 			<div class="entry">
 				<?php if ( get_option('woo_blog_excerpt') == "true" ) { the_excerpt() ?>
 					<p><span class="continue"><a title="<?php _e('Permalink to ','woothemes'); ?> <?php the_title(); ?>" href="<?php the_permalink() ?>"><?php _e('Read the full story','woothemes'); ?></a></span></p>
