ba08d87c04e32f63d3336a8b944dc8d887bb6d76
Darren Li <darren@greatwallclub.com>
2011-12-29 13:40:11 +0800

    update footer link to en.greatwallclub.com

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	
 	<div id="footer">
 		 <p class="fl">&copy; <?php echo date('Y'); ?> <?php bloginfo(); ?>. </p>
-            <p class="fr"><a href="http://www.greatwallclub.com">Great Wall Club</a></p>
+            <p class="fr"><a href="http://en.greatwallclub.com">Great Wall Club</a></p>
 	</div><!--/footer -->
 
 </div><!--/page -->
